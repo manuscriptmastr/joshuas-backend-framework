c83d8870c79e526d13dce9aa18d1c3076bc77c2c
Joshua Martin <the.manuscriptmaster@gmail.com>
2024-08-15 16:03:45 -0400

    Koa implementation, pt. 1

--- src/controllers/home.js
+++
@@ -1,9 +0,0 @@
-import { controller, get } from "../utils/decorators.js";
-
-@controller
-export class HomeController {
-  @get("/")
-  getLandingPage(req, res) {
-    res.write("Hello world");
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import { createServer } from "node:http";
-import { HomeController } from "./controllers/home.js";
-import { UserController } from "./controllers/user.js";
 
-const server = createServer((req, res) => {
-  new HomeController().handle(req, res);
-  new UserController().handle(req, res);
-  res.end();
+const createApp = () => {
+  const stack = [];
+
+  const app = (req, res) => {
+    const [top = (i) => i] = stack;
+    const { res: response } = top({ req, res });
+    response.end();
+  };
+
+  app.use = (fn) => {
+    const middleware = (ctx) =>
+      fn(ctx, stack[stack.indexOf(middleware) + 1] ?? ((i) => i));
+    return stack.push(middleware);
+  };
+
+  return app;
+};
+
+const app = createApp();
+
+app.use(({ req, res }, next) => {
+  console.log("Middleware 1", req.url);
+  return next({ req, res });
 });
+
+app.use(({ req, res }, next) => {
+  console.log("Middleware 2:incoming", req.url);
+  res.write("Hello World");
+  const context = next({ req, res });
+  console.log("Middleware 2:outgoing", req.url);
+  return context;
+});
+
+const server = createServer(app);
 
 server.listen(3000);
